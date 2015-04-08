c4ddfce1064d85c6ec62ccaeb2f613ad957d2412
Daan Debie <d.debie@thenewmotion.com>
2015-04-08 15:09:29 +0200

    Refactor auth, PEP8 compliance, better HTTP error codes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os.path
+
 from flask import Flask, request
 from impala.dbapi import connect
 from impala.error import Error
@@ -14,38 +15,46 @@ def query_impala(sql):
     field_names = [f[0] for f in cursor.description]
     return result, field_names
 
+
 def query_impala_cursor(sql, params=None):
     conn = connect(host=config['IMPALA_HOST'], port=config['IMPALA_PORT'])
     cursor = conn.cursor()
     cursor.execute(sql.encode('utf-8'), params)
     return cursor
 
+
 def result2csv(records, column_names, include_header):
     if include_header:
         records.insert(0, column_names)
-    list_of_str = [','.join(map(str,rec)) for rec in records]
+    list_of_str = [','.join(map(str, rec)) for rec in records]
     csv = '\n'.join(list_of_str)
     return csv
+
 
 def is_select(sql):
     return sql.strip().upper().startswith("SELECT")
 
+
 def str_is_true(string):
     return string.lower() in ['true', '1', 't', 'y', 'yes']
 
+
+@app.before_request
+def authenticate():
+    token = request.args.get('token', '')
+    # FIXME: Use Google API authentication instead
+    if token != config['SECURITY_TOKEN']:
+        return "Unauthorized", 401
+
+
 @app.route("/impala")
 def impala():
-    token = request.args.get('token', '')
     sql_query = request.args.get('q', '')
     include_column_names = str_is_true(request.args.get('header', ''))
 
-    # FIXME: Use Google API authentication instead
-    if token != config['SECURITY_TOKEN']:
-        return "Unauthorized", 422
-
     # FIXME: Use proper permission setting, e.g. Apache Sentry
     if not is_select(sql_query):
-        return "Only SELECT queries are allowed", 422
+        return "Only SELECT queries are allowed", 403
 
     records, column_names = query_impala(sql_query)
 
@@ -62,13 +71,15 @@ def impala():
 @app.errorhandler(Error)
 @app.errorhandler(Exception)
 def handle_invalid_usage(error):
-    return error.message.replace('AnalysisException: ',''), 400
+    return error.message.replace('AnalysisException: ', ''), 400
+
 
 def load_config_if_exists(fname):
     if os.path.isfile(fname):
         app.config.from_pyfile(fname)
     else:
-        print "Config file {0} does not exist, skippping".format(fname)
+        print "Config file {0} does not exist, skipping".format(fname)
+
 
 if __name__ == "__main__":
     load_config_if_exists('reference.cfg')
